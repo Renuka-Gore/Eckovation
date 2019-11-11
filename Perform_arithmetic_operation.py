3604607fce9fc2e7ac317974287662953d6b163b
Renuka-Gore <56173697+Renuka-Gore@users.noreply.github.com>
2019-11-11 17:25:31 +0530

    Add files via upload

---
+++ Perform_arithmetic_operation.py
@@ -0,0 +1,15 @@
+#take the inputs
+n1 = int(input())
+n2 = int(input())
+
+#perform calculations
+nsum = n1+n2
+ndiff = n1-n2
+nmult = n1*n2
+
+#display results
+
+#In next 3 statements, replace '#' with appropriate command
+print('Sum of the two numbers is: ', nsum)
+print('Difference of the two numbers is: ', ndiff)
+print('Product of the two numbers is: ' , nmult)
