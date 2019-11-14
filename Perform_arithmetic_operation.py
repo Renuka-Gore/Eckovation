ddfdf45dac1169c661cdf35c8827648e79755037
RENUKA GORE <renuka73gore@yahoo.in>
2019-11-14 22:45:39 +0530

    Update Perform_arithmetic_operation.py
    Basic arithmetic operation with exception handling

--- Perform_arithmetic_operation.py
+++ Perform_arithmetic_operation.py
@@ -7,9 +7,16 @@ nsum = n1+n2
 ndiff = n1-n2
 nmult = n1*n2
 
+def div():
+    try:
+       return n1/n2
+    except ZeroDivisionError:
+        print("Can not be devided by 0!")
+
 #display results
 
 #In next 3 statements, replace '#' with appropriate command
 print('Sum of the two numbers is: ', nsum)
 print('Difference of the two numbers is: ', ndiff)
 print('Product of the two numbers is: ' , nmult)
+print('Divison of two numbers is: ', div())
